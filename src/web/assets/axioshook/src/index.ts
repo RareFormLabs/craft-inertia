1e0ddb0adbc1227cb0bf01317d7a8170c2a0f767
Chase Giunta <me@chasegiunta.com>
2025-04-13 21:40:47 -0500

    handle un/authorized meta tag handshake

--- src/web/assets/axioshook/src/index.ts
+++ src/web/assets/axioshook/src/index.ts
@@ -1,17 +1,41 @@
-const getSessionInfo = async function () {
-  return await fetch("/actions/users/session-info", {
-    headers: {
-      Accept: "application/json",
-    },
+import type { AxiosInstance } from "axios";
+
+declare global {
+  interface Window {
+    axios: any;
+  }
+}
+
+type SessionInfo = {
+  csrfTokenName: string;
+  csrfTokenValue: string;
+  isGuest: boolean;
+  timeout: number;
+  email?: string;
+  id?: number;
+  uid?: string;
+  username?: string;
+};
+
+type csrfMeta = {
+  csrfTokenName: string;
+  csrfTokenValue: string;
+};
+
+const CSRF_ENDPOINT = "/actions/users/session-info";
+
+const getSessionInfo = async function (): Promise<SessionInfo> {
+  return await fetch(CSRF_ENDPOINT, {
+    headers: { Accept: "application/json" },
   }).then((response) => response.json());
 };
 
 // Don't store the promise, store the session info once it's resolved
-let sessionInfo = null;
+let sessionInfo: SessionInfo | null = null;
 
-const getActionPath = (url) => {
-  const postPathObject = URL.parse(url);
-  const postPathPathname = postPathObject.pathname;
+const getActionPath = (url: string) => {
+  const postPathObject: URL = new URL(url);
+  const postPathPathname: string = postPathObject.pathname;
 
   // Get window.location.pathname without the last part
   const locationPathParts = window.location.pathname.split("/");
@@ -26,45 +50,72 @@ const getActionPath = (url) => {
   return pathWithoutSlash;
 };
 
+// const storeCsrfOnMeta = (csrfTokenName, csrfTokenValue) => {
+
+const getTokenFromMeta = (): csrfMeta | null => {
+  const csrfMetaEl = document.head.querySelector("meta[csrf]");
+  if (!csrfMetaEl) {
+    return null;
+  }
+  const tokenName = csrfMetaEl?.getAttribute("name");
+  const tokenValue = csrfMetaEl?.getAttribute("content");
+  return {
+    csrfTokenName: tokenName ?? "CRAFT_CSRF_TOKEN",
+    csrfTokenValue: tokenValue ?? "",
+  };
+};
+
+const setCsrfOnMeta = (csrfTokenName: string, csrfTokenValue: string): void => {
+  // Check if a CSRF meta element already exists
+  let csrfMetaEl = document.head.querySelector("meta[csrf]");
+
+  if (csrfMetaEl) {
+    // Update existing meta element
+    csrfMetaEl.setAttribute("name", csrfTokenName);
+    csrfMetaEl.setAttribute("content", csrfTokenValue);
+  } else {
+    // Create and append a new meta element
+    csrfMetaEl = document.createElement("meta");
+    csrfMetaEl.setAttribute("csrf", "");
+    csrfMetaEl.setAttribute("name", csrfTokenName);
+    csrfMetaEl.setAttribute("content", csrfTokenValue);
+    document.head.appendChild(csrfMetaEl);
+  }
+};
+
 const configureAxios = async () => {
-  /** Get Token From Meta Tag
-    const getCsrfMetaEl = () => document.head.querySelector("meta[csrf]");
-
-    const getCsrfName = (csrfMeta) => {
-      const meta = csrfMeta ?? getCsrfMetaEl();
-      const tokenName = meta?.getAttribute("name");
-      return tokenName ?? null;
-    };
-
-    const getCsrfToken = (csrfMeta) => {
-      const meta = csrfMeta ?? getCsrfMetaEl();
-      const tokenValue = meta?.getAttribute("content");
-      return tokenValue;
-    };
-    */
-
   window.axios.defaults.headers = {
     "Content-Type": "multipart/form-data",
   };
 
-  window.axios.interceptors.request.use(async (config) => {
+  (window.axios as AxiosInstance).interceptors.request.use(async (config) => {
     if (config.method === "post" || config.method === "put") {
-      // Wait for the session info to be resolved before configuring axios
-      sessionInfo = await getSessionInfo();
+      let csrfMeta = getTokenFromMeta();
+      if (!csrfMeta) {
+        // Wait for the session info to be resolved before configuring axios
+        sessionInfo = await getSessionInfo();
+        if (!sessionInfo.isGuest) {
+          setCsrfOnMeta(sessionInfo.csrfTokenName, sessionInfo.csrfTokenValue);
+          csrfMeta = getTokenFromMeta();
+        }
+      }
+
+      const csrf = csrfMeta || sessionInfo;
+
+      if (!csrf) {
+        throw new Error("CSRF token not found");
+      }
 
       const actionPath = getActionPath(config.url);
 
       config.url = "";
 
       if (config.data instanceof FormData) {
-        config.data.append(
-          sessionInfo.csrfTokenName,
-          sessionInfo.csrfTokenValue
-        );
+        config.data.append(csrf.csrfTokenName, csrf.csrfTokenValue);
         config.data.append("action", actionPath);
       } else {
         config.data = {
-          [sessionInfo.csrfTokenName]: sessionInfo.csrfTokenValue,
+          [csrf.csrfTokenName]: csrf.csrfTokenValue,
           action: actionPath,
           ...config.data,
         };
@@ -72,6 +123,18 @@ const configureAxios = async () => {
     }
     return config;
   });
+
+  // Add a response interceptor
+  (window.axios as AxiosInstance).interceptors.response.use(
+    async (response) => {
+      if (response.config.data.get("action") == "users/login") {
+        await getSessionInfo().then((sessionInfo) => {
+          setCsrfOnMeta(sessionInfo.csrfTokenName, sessionInfo.csrfTokenValue);
+        });
+      }
+      return response;
+    }
+  );
 };
 
 const checkForAxios = async () => {
--- src/web/assets/axioshook/vite.config.ts
+++ src/web/assets/axioshook/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
+    // minify: false,
     lib: {
       name: "axioshook",
       entry: ["src/index.ts"],
